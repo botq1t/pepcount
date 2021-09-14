22179f22dddd3adfb751c688c4937611f6622e89
botq1t <77788765+botq1t@users.noreply.github.com>
2021-09-14 13:25:51 +0300

    version 2_0

--- #source/js/main.js
+++ #source/js/main.js
@@ -1,176 +1,302 @@
-let time, date, passed, passedInSeconds;
-localStorage['start'] = localStorage['start'] ?? 'false';
-localStorage['stop'] = localStorage['stop'] ?? 'false';
-localStorage['card-1'] = localStorage['card-1'] ?? 0;
-localStorage['card-2'] = localStorage['card-2'] ?? 0;
-localStorage['card-3'] = localStorage['card-3'] ?? 0;
-
-getTime();
-setInterval(getTime, 500);
-
-if (localStorage['start'] == 'true') {
-	setBeginTimer();
-
-	displayCounter('card-1');
-	displayCounter('card-2');
-	displayCounter('card-3');
-	updateFrequency('card-1');
-	updateFrequency('card-2');
-	updateFrequency('card-3');
-	updateTimer();
-	setInterval(updateTimer, 1000);
-	setInterval(updateFrequency, 1000, 'card-1');
-	setInterval(updateFrequency, 1000, 'card-2');
-	setInterval(updateFrequency, 1000, 'card-3');
-} else {
-	localStorage['beginTimer'] = null;
-	localStorage['card-1'] = 0;
-	localStorage['card-2'] = 0;
-	localStorage['card-3'] = 0;
+'use strict'
+let parametersDefault = {
+	timeBegin: 0,
+	timePauseStart: 0,
+	timePauseNow: 0,
+	timePause: 0,
+	timePassing: 0,
 }
-checkStart()
+
+if (!localStorage['parameters']) localStorage['parameters'] = JSON.stringify(parametersDefault);
+let parameters = JSON.parse(localStorage['parameters']);
+console.log('Parameters', parameters);
+
+let counterDefault = [];
+
+$('.card').each(function () {
+	counterDefault.push(0);
+});
+
+if (!localStorage['counter']) localStorage['counter'] = JSON.stringify(counterDefault);
+let counter = JSON.parse(localStorage['counter']);
+console.log('counter', counter);
+
+if (!localStorage['mode']) localStorage['mode'] = 'stop';
+console.log(localStorage);
+
+let loggerDefault = [];
+
+if (!localStorage['logger']) localStorage['logger'] = JSON.stringify(loggerDefault);
+let logger = JSON.parse(localStorage['logger']);
+
+let timeCurrent;
+let timePassing;
+let intervalTimer, intervalPause;
+checkMode(localStorage['mode']);
+displayLogger('launch');
+setCounter();
 
 $('.main__button_start').click(function () {
-	localStorage['start'] = 'true';
-	localStorage['beginTimer'] = time;
-	console.log('click start', localStorage['start']);
-	$('.main__button_end').html('Закончить подсчёт!');
+	localStorage['mode'] = 'start';
+	timer();
+	parameters.timeBegin = timeCurrent;
+	localStorage['parameters'] = JSON.stringify(parameters);
+	checkMode(localStorage['mode']);
 
-	setBeginTimer();
+	logger.push({
+		time: timeCurrent,
+		timeString: getTimeString(timeCurrent),
+		meaning: 'start',
+		action: 'start',
+	});
+	localStorage['logger'] = JSON.stringify(logger);
+	console.log('logger', logger);
+	$('.main__logger').empty();
+	displayLogger('now');
+});
 
-	displayCounter('card-1');
-	displayCounter('card-2');
-	displayCounter('card-3');
+$('.main__button_pause').click(function () {
+	switch ($(this).html()) {
+		case 'Пауза':
+			localStorage['mode'] = 'pause';
+			parameters.timePauseStart = timeCurrent;
+			parameters.timePassing = timeCurrent - parameters.timeBegin - parameters.timePauseNow;
+			localStorage['parameters'] = JSON.stringify(parameters);
 
-	updateTimer();
-	setInterval(updateTimer, 1000);
-	setInterval(updateFrequency, 1000, 'card-1');
-	setInterval(updateFrequency, 1000, 'card-2');
-	setInterval(updateFrequency, 1000, 'card-3');
-	checkStart();
-})
+			logger.push({
+				time: timeCurrent,
+				timeString: getTimeString(timeCurrent),
+				meaning: 'pause',
+				action: 'pause',
+			});
+			localStorage['logger'] = JSON.stringify(logger);
+			console.log('logger', logger);
+			displayLogger('now');
+			break;
+		case 'Продолжить':
+			localStorage['mode'] = 'start';
+			parameters.timePauseStart = 0;
+			parameters.timePause = parameters.timePauseNow;
+			localStorage['parameters'] = JSON.stringify(parameters);
+
+			logger.push({
+				time: timeCurrent,
+				timeString: getTimeString(timeCurrent),
+				meaning: 'resume',
+				action: 'resume',
+			});
+			localStorage['logger'] = JSON.stringify(logger);
+			console.log('logger', logger);
+			displayLogger('now');
+			break;
+	}
+
+	checkMode(localStorage['mode']);
+});
 
 $('.main__button_end').click(function () {
-	localStorage['start'] = 'false';
-	console.log('click end', localStorage['start']);
-	$(this).fadeOut(300);
-	// checkStart();
-})
-// ? ======================================= Card 1 ==========================================
-$('#card-1').children('.card__controls').children('.card__button_plus').click(function () {
-	setCardPlus('card-1');
-})
-$('#card-1').children('.card__controls').children('.card__button_minus').click(function () {
-	setCardMinus('card-1');
-})
+	localStorage['mode'] = 'stop';
 
-// ? ======================================= Card 2 ==========================================
-$('#card-2').children('.card__controls').children('.card__button_plus').click(function () {
-	setCardPlus('card-2');
-})
-$('#card-2').children('.card__controls').children('.card__button_minus').click(function () {
-	setCardMinus('card-2');
-})
-// ? ======================================= Card 3 ==========================================
-$('#card-3').children('.card__controls').children('.card__button_plus').click(function () {
-	setCardPlus('card-3');
-})
-$('#card-3').children('.card__controls').children('.card__button_minus').click(function () {
-	setCardMinus('card-3');
-})
-// ! ====================== Functions =====================================
-function getTime() {
-	date = new Date();
-	time = date.getHours() * 3600 + date.getMinutes() * 60 + date.getSeconds();
+	checkMode(localStorage['mode']);
+	setCounter();
+});
 
-}
+$('.card__button_plus').click(function () {
+	let id = $(this).parent().parent().attr('id');
+	id = id.split('_')[1];
+	counter[id]++;
+	localStorage['counter'] = JSON.stringify(counter);
+	console.log(id);
+	setCounter(id);
 
-function checkStart() {
-	if (localStorage['start'] == "true") {
-		$('.main__start').fadeOut(300);
-		$('.card').css('display', 'flex');
-		$('.main__controls').css('display', 'flex');
-	} else {
-		$('.main__start').fadeIn(300, function () {
-			$(this).css('display', 'flex')
-		});
-		$('.card').css('display', 'none');
-		$('.main__controls').css('display', 'none');
+	logger.push({
+		time: timeCurrent,
+		counter: counter[id],
+		timeString: getTimeString(timeCurrent),
+		meaning: $(this).parent().prev().prev().html(),
+		action: '+',
+	});
+	localStorage['logger'] = JSON.stringify(logger);
+	displayLogger('now');
+});
+
+$('.card__button_minus').click(function () {
+	let id = $(this).parent().parent().attr('id');
+	id = id.split('_')[1];
+	if (counter[id] == 0) return;
+
+	counter[id]--;
+	localStorage['counter'] = JSON.stringify(counter);
+	console.log(id);
+	setCounter(id);
+
+	logger.push({
+		time: timeCurrent,
+		counter: counter[id],
+		timeString: getTimeString(timeCurrent),
+		meaning: $(this).parent().prev().prev().html(),
+		action: '-',
+	});
+	localStorage['logger'] = JSON.stringify(logger);
+	displayLogger('now');
+});
+
+
+
+
+
+function checkMode(mode) {
+	switch (mode) {
+		case 'stop':
+			localStorage['parameters'] = JSON.stringify(parametersDefault);
+			localStorage['counter'] = JSON.stringify(counterDefault);
+			localStorage['logger'] = JSON.stringify(loggerDefault);
+			parameters = JSON.parse(localStorage['parameters']);
+			counter = JSON.parse(localStorage['counter']);
+			logger = JSON.parse(localStorage['logger']);
+			clearInterval(intervalTimer);
+			clearInterval(intervalPause);
+			$('.main__start').css('display', 'flex');
+			break;
+		case 'start':
+			$('.main__start').css('display', 'none');
+			$('.main__button_end').css('display', 'none');
+			$('.main__button_pause').html('Пауза');
+			$('.timer__begin').children('span').html(getTimeString(parameters.timeBegin));
+
+			timer();
+			intervalTimer = setInterval(timer, 1000);
+			clearInterval(intervalPause);
+			break;
+		case 'pause':
+			$('.main__start').css('display', 'none');
+			$('.main__button_end').css('display', 'flex');
+			$('.main__button_pause').html('Продолжить');
+			$('.timer__begin').children('span').html(getTimeString(parameters.timeBegin));
+			$('.timer__passing').children('span').html(getTimeString(parameters.timePassing));
+
+			getPause();
+			timer();
+			getFrequency(parameters.timePassing);
+			intervalPause = setInterval(getPause, 500);
+			intervalTimer = setInterval(timer, 1000);
+			break;
+
 	}
 }
 
-function setBeginTimer() {
-	let timer = getHMS(localStorage['beginTimer']);
-	timer = getTimeString(timer['hours'], timer['minutes'], timer['seconds']);
-	$('.timer__begin').children('span').html(timer);
-}
-
-function updateTimer() {
-	if (localStorage['start'] == 'false') {
-		passed = localStorage['passed'];
-	} else {
-		passed = time - localStorage['beginTimer'];
-		localStorage['passed'] = passed;
-	}
-	passedInSeconds = passed;
-	passed = getHMS(passed);
-	let output = $('.timer__passing').children('span');
-	output.empty();
-	if (passed['hours'] > 0) {
-		output.append(`${passed['hours']} ч, ${passed['minutes']} мин, ${passed['seconds']} сек`);
-	} else if (passed['minutes'] > 0) {
-		output.append(`${passed['minutes']} мин, ${passed['seconds']} сек`);
-
-	} else {
-		output.append(`${passed['seconds']} сек`);
+function timer() {
+	let date = new Date();
+	timeCurrent = date.getHours() * 3600 + date.getMinutes() * 60 + date.getSeconds();
+	timePassing = timeCurrent - parameters.timeBegin - parameters.timePauseNow;
+	if (localStorage['mode'] == 'start') {
+		$('.timer__passing').children('span').html(getTimeString(timePassing));
+		getFrequency(timePassing);
 	}
 }
 
-function setCardPlus(card) {
-	localStorage[card]++;
-	displayCounter(card);
-}
+function getTimeString(time) {
+	let timeString = '';
+	let hours = Math.floor(time / 3600);
+	let minutes = Math.floor((time - hours * 3600) / 60);
+	let seconds = Math.floor(time - hours * 3600 - minutes * 60);
 
-function setCardMinus(card) {
-	localStorage[card]--;
-	displayCounter(card);
-}
-
-function displayCounter(card) {
-	$(`#${card}`).children('.card__displayer').html(localStorage[card]);
-}
-
-function updateFrequency(card) {
-	let frequency = $(`#${card}`).children('.frequency');
-	let sec = localStorage[card] / passedInSeconds;
-
-	frequency.children('.freq-second').children('span').html(sec.toFixed(2));
-	frequency.children('.freq-minute').children('span').html((sec * 60).toFixed(2));
-	frequency.children('.freq-hour').children('span').html((sec * 3600).toFixed(2));
-
-}
-
-function getHMS(time) {
-	var time;
-	var output = {
-		'hours': 0,
-		'minutes': 0,
-		'seconds': 0
-	}
-	output['hours'] = Math.floor(time / 3600);
-	output['minutes'] = Math.floor((time - (output['hours'] * 3600)) / 60);
-	output['seconds'] = time - (output['hours'] * 3600) - (output['minutes'] * 60);
-	return output;
-}
-
-function getTimeString(h, m, s) {
-	var h, m, s;
-	var timeString = ''
-	if (h < 10) { timeString = timeString + '0' }
-	timeString = timeString + h + ':';
-	if (m < 10) { timeString = timeString + '0' }
-	timeString = timeString + m + ':';
-	if (s < 10) { timeString = timeString + '0' }
-	timeString = timeString + s;
+	if (hours < 10) timeString = timeString + '0';
+	timeString = timeString + `${hours}:`;
+	if (minutes < 10) timeString = timeString + '0';
+	timeString = timeString + `${minutes}:`;
+	if (seconds < 10) timeString = timeString + '0';
+	timeString = timeString + `${seconds}`;
 	return timeString;
+}
+
+function getPause() {
+	parameters.timePauseNow = parameters.timePause + (timeCurrent - parameters.timePauseStart);
+}
+
+function setCounter(id) {
+	if (!id) {
+		$('.card').each(function () {
+			let id = $(this).attr('id');
+			id = id.split('_')[1];
+			$(this).children('.card__controls').children('.card__displayer').html(counter[id]);
+		})
+	} else {
+		$(`#card_${id}`).children('.card__controls').children('.card__displayer').html(counter[id]);
+	}
+}
+
+function getFrequency(time) {
+	$('.card').each(function () {
+		let id = $(this).attr('id');
+		id = id.split('_')[1];
+		let freq = {
+			h: 0,
+			m: 0,
+			s: 0,
+		};
+		freq.s = counter[id] / time;
+		freq.m = freq.s * 60;
+		freq.h = freq.m * 60;
+
+
+		// console.log(freq);
+		$(this).children('.frequency').children('.freq-hour').children('span').html(freq.h.toFixed(2));
+		$(this).children('.frequency').children('.freq-minute').children('span').html(freq.m.toFixed(2));
+		$(this).children('.frequency').children('.freq-second').children('span').html(freq.s.toFixed(2));
+
+	})
+}
+
+function displayLogger(mode) {
+	switch (mode) {
+		case 'launch':
+			$('.main__logger').empty();
+			for (let i = 0; i < logger.length; i++) {
+				if (i > 0 && (logger[i].time - logger[i - 1].time) >= 10) {
+					$('.main__logger').prepend('<p class="logger__spacing"></p>');
+				}
+
+				switch (logger[i].action) {
+					case '+':
+						$('.main__logger').prepend(`<p><span class="logger__time">[${logger[i].timeString}]</span> ${logger[i].meaning} (добавлено) <span class="logger__blue">[${logger[i].counter}]</span></p>`)
+						break;
+					case '-':
+						$('.main__logger').prepend(`<p><span class="logger__time">[${logger[i].timeString}]</span> ${logger[i].meaning} (Убрано) <span class="logger__blue">[${logger[i].counter}]</span></p>`)
+						break;
+					case 'start':
+						$('.main__logger').prepend(`<p><span class="logger__time">[${logger[i].timeString}]</span> <span class="logger__blue">Отсчёт начался</span></p>`)
+						break;
+					case 'pause':
+						$('.main__logger').prepend(`<p><span class="logger__time">[${logger[i].timeString}]</span> <span class="logger__blue">Пауза</span></p>`)
+						break;
+					case 'resume':
+						$('.main__logger').prepend(`<p><span class="logger__time">[${logger[i].timeString}]</span> <span class="logger__blue">Продолжение отсчёта</span></p>`)
+						break;
+				}
+			}
+			break;
+		case 'now':
+			let i = logger.length - 1;
+			if (i > 0 && (logger[i].time - logger[i - 1].time) >= 10) $('.main__logger').prepend('<p class="logger__spacing"></p>');
+
+			switch (logger[i].action) {
+				case '+':
+					$('.main__logger').prepend(`<p><span class="logger__time">[${logger[i].timeString}]</span> ${logger[i].meaning} (добавлено) <span class="logger__blue">[${logger[i].counter}]</span></p>`)
+					break;
+				case '-':
+					$('.main__logger').prepend(`<p><span class="logger__time">[${logger[i].timeString}]</span> ${logger[i].meaning} (Убрано) <span class="logger__blue">[${logger[i].counter}]</span></p>`)
+					break;
+				case 'start':
+					$('.main__logger').prepend(`<p><span class="logger__time">[${logger[i].timeString}]</span> <span class="logger__blue">Отсчёт начался</span></p>`)
+					break;
+				case 'pause':
+					$('.main__logger').prepend(`<p><span class="logger__time">[${logger[i].timeString}]</span> <span class="logger__blue">Пауза</span></p>`)
+					break;
+				case 'resume':
+					$('.main__logger').prepend(`<p><span class="logger__time">[${logger[i].timeString}]</span> <span class="logger__blue">Продолжение отсчёта</span></p>`)
+					break;
+			}
+			break;
+	}
 }
